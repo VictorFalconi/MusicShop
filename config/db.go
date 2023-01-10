d05da3a3676e174af3952a2820ea4f97d378b181
Victor Falconi <v.falconi.m@gmail.com>
2023-01-10 19:15:26 +0700

    Update docker-compose.yml, Dockerfile: Run server and db in Docker

--- config/db.go
+++ config/db.go
@@ -11,7 +11,9 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := os.Getenv("DB_URL")
+	//dsn := os.Getenv("DB_URL") "postgres://thanhliem:liem1234@localhost:41234/musicshop"
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("POSTGRES_DB"))
+	fmt.Println(dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
