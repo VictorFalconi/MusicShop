6c388f0486f8b590d53b009ef8f800fb88a18de7
Victor Falconi <v.falconi.m@gmail.com>
2023-02-17 14:49:07 +0700

    Update CORS: Enable CORS for AJAX request

--- main.go
+++ main.go
@@ -15,25 +15,8 @@ func init() {
 func main() {
 	router := gin.Default()
 
-	// Allow CORS with credentials
+	// Allow CORS middleware with credentials
 	router.Use(middleware.CorsMiddleware())
-
-	//// Use the CORS middleware
-	//config := cors.DefaultConfig()
-	//config.AllowOrigins = []string{"http://localhost:3000"}
-	//config.AllowCredentials = true
-	//config.ExposeHeaders = []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"}
-	//config.AllowMethods = []string{"POST, GET, OPTIONS, PUT, DELETE"}
-	////config := cors.Config{
-	////	AllowOrigins:     []string{"http://localhost:3000"},
-	////	AllowCredentials: true,
-	////	ExposeHeaders:    []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"},
-	////	AllowMethods:     []string{"POST, GET, OPTIONS, PUT, DELETE"},
-	////	//AllowHeaders:     []string{"Origin"},
-	////	//AllowOriginFunc: func(origin string) bool {return origin == "https://github.com"},
-	////	//MaxAge: 12 * time.Hour,
-	////}
-	//router.Use(cors.New(config))
 
 	// Routes
 	routes.UserRouter(router)
