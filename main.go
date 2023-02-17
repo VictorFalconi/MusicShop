1bbe5ae97317923fecaa75ae3d3736d6e29ed96a
Victor Falconi <v.falconi.m@gmail.com>
2023-02-17 14:38:35 +0700

    Update CORS: Enable CORS for AJAX request

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"server/app/routes"
 	"server/config"
-	"server/middleware"
 )
 
 func init() {
@@ -14,22 +13,28 @@ func init() {
 }
 
 func main() {
-	//router := gin.New()
 	router := gin.Default()
 
-	// Allow CORS with credentials
-	router.Use(middleware.CorsMiddleware())
+	//// Allow CORS with credentials
+	//router.Use(middleware.CorsMiddleware())
+
+	// Use the CORS middleware
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"},
+		AllowMethods:     []string{"POST, GET, OPTIONS, PUT, DELETE"},
+		//AllowHeaders:     []string{"Origin"},
+		//AllowOriginFunc: func(origin string) bool {return origin == "https://github.com"},
+		//MaxAge: 12 * time.Hour,
+	}
+	router.Use(cors.New(config))
 
 	// Routes
 	routes.UserRouter(router)
 	routes.BrandRouter(router)
 	routes.ProductRouter(router)
 	routes.OrderRouter(router)
-
-	// Use the CORS middleware
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
-	router.Use(cors.New(config))
 
 	// Start the server
 	if err := router.Run(); err != nil {
