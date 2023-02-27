915fd50bf435d5a552ae26b5cb7a0d5ae1938270
Victor Falconi <v.falconi.m@gmail.com>
2023-02-27 16:16:59 +0700

    Update version 2: Custom for Axios frontend

--- middleware/auth.go
+++ middleware/auth.go
@@ -25,7 +25,7 @@ func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, x-xsrf-token, Authorization")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(204)
